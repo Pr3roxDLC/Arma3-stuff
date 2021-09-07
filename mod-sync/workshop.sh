4de273fabc8cbb5cea8aff58ffb43c7088e99376
Pr3roxDLC <57639545+Pr3roxDLC@users.noreply.github.com>
2021-09-07 16:18:33 +0200

    Made it work for my server env

--- mod-sync/workshop.sh
+++ mod-sync/workshop.sh
@@ -34,20 +34,16 @@ if [ ! -d "$home"/mods ]; then
         exit 1
     fi
 fi
-STEAMPASS_decrypted=$(echo "${STEAMPASS}" | openssl enc -a -d -aes-256-cbc -md md5 -pass pass:"${CRYPTKEY}")
+STEAMPASS_decrypted="${STEAMPASS}"
 for workshop_item in "${WS_IDS[@]}"; do
     modname="$(curl -s https://steamcommunity.com/sharedfiles/filedetails/?id="${workshop_item}" | grep "<title>" | sed -e 's/<[^>]*>//g' | cut -d ' ' -f 4-)"
     modname_clean=$(echo "$modname" | dos2unix)
     counter=1
     printf "Downloading %s \n" "$modname_clean"
-    until steamcmd +login "${STEAMUSER}" "${STEAMPASS_decrypted}" +workshop_download_item 107410 "${workshop_item}" validate +quit; do
+    until /home/steam/steamcmd/steamcmd.sh +login "${STEAMUSER}" "${STEAMPASS_decrypted}" +workshop_download_item 107410 "${workshop_item}" validate +quit; do
         printf "Error Downloading %s. Will try again \n" "$modname_clean"
-        ((counter++))
-        if ((counter > 4)); then
-            exit 1
-        fi
     done
     if [ ! -L "${home}/mods/@${modname_clean}" ]; then
-        ln -s "${home}/.steam/steamapps/workshop/content/107410/${workshop_item}/" "${home}/mods/@${modname_clean}"
+        ln -s "${home}/Steam/steamapps/workshop/content/107410/${workshop_item}/" "${home}/mods/@${modname_clean}"
     fi
 done
